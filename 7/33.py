ac429bfeca0946cb69276072d6ff36657afa7600
Anna <pestova431@gmail.com>
2026-09-25 10:59:49 +0300

    Начало работы6

--- 7/33.py
+++ 7/33.py
@@ -5,5 +5,5 @@ list_text=list(text)
 result=[item for item in list_text if item not in vowels]
 #text1=set(text)
 result_str=(''.join(result))
-#print(result_str)
+print(result_str)
 print(len(result_str))
